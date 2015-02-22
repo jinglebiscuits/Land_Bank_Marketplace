5e6de988d380f85e04c298e293bd9b5134fbf964
jamall harris <jharri34@uab.edu>
2015-02-22 13:18:19 -0600

    working on search bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,7 +23,7 @@ function initialize() {
     google.maps.event.addDomListener(document.getElementById('input'), 'keydown', function (e) {
         if (e.keyCode == 13) {
             var input = document.getElementById('input').value;
-            console.log(document.getElementById('input').value);
+            //            console.log(document.getElementById('input').value);
 
             updateMap(layer, tableID, input)
             e.preventDefault();
@@ -36,31 +36,37 @@ function initialize() {
     layer.setMap(map);
 }
 
-function updateMap(layer, tableID) {
-    var loc = document.getElementById('input').value;
-    if (loc) {
-        console.log("hey this happen");
-        layer.setOptions({
+function updateMap(oldLayer, tableID) {
+    
+    //    var loc = document.getElementById('input').value;
+    var input = document.getElementById('input').value;
+    var addressQuery = encodeURIComponent(" SELECT 'FULL_ADDRESS' " + " FROM " + tableID + " WHERE ('FULL_ADDRESS' LIKE '%" + input + "%')");
+    //    var parcelQuery = encodeURIComponent(" SELECT PARCELID " + " FROM " + tableID + " WHERE ('PARCELID' LIKE \'% " + input + " %\')");
+//    var query = null;
+//    var results = [];
+    console.log(input);
+    if (typeof input == "string") {
+        var newLayer = new google.maps.FusionTablesLayer({
             query: {
-                select: loc,
+                select:'FULL_ADDRESS' ,
                 from: tableID,
-                like: loc
+                where: "ASSESSEDTO > 1000"
             }
         });
+        if (newLayer)
+        console.log(newLayer);
+        oldLayer.setMap(null);
+        newLayer.setMap(map);
     } else {
-        layer.setOptions({
-            query: {
-                select: loc,
-                from: tableID
-            }
-        });
+        console.log("huh?!?!");
     }
+
 }
 
 function drawTable() {
         var queryText = encodeURIComponent(
             " SELECT 'FULL_ADDRESS' as address,'PARCELID' as id, 'ASSESSEDTO' as value " + " FROM " + tableID);
-        console.log(queryText);
+        //        console.log(queryText);
         var query = new google.visualization.Query(
             'http://www.google.com/fusiontables/gvizdata?tq=' + queryText);
 
@@ -76,27 +82,27 @@ function drawTable() {
             var numCols = response.getDataTable().getNumberOfColumns();
             var ftdata = ['<table class="table table-striped table-hover "><thead><tr>'];
             for (var i = 0; i < numCols; i++) {
-            var columnTitle = response.getDataTable().getColumnLabel(i);
-            ftdata.push('<th>' + columnTitle + '</th>');
-          }
-          ftdata.push('</tr></thead><tbody>');
-            for (var i = 0; i < numRows; i++) {
-            ftdata.push('<tr>');
-            for(var j = 0; j < numCols; j++) {
-              var rowValue = response.getDataTable().getValue(i, j);
-              ftdata.push('<td>' + rowValue + '</td>');
+                var columnTitle = response.getDataTable().getColumnLabel(i);
+                ftdata.push('<th>' + columnTitle + '</th>');
             }
-            ftdata.push('</tr>');
-          }
-          ftdata.push('</tbody></table>');
-          document.getElementById('ft-data').innerHTML = ftdata.join('');
+            ftdata.push('</tr></thead><tbody>');
+            for (var i = 0; i < numRows; i++) {
+                ftdata.push('<tr>');
+                for (var j = 0; j < numCols; j++) {
+                    var rowValue = response.getDataTable().getValue(i, j);
+                    ftdata.push('<td>' + rowValue + '</td>');
+                }
+                ftdata.push('</tr>');
+            }
+            ftdata.push('</tbody></table>');
+            document.getElementById('ft-data').innerHTML = ftdata.join('');
 
             // Create the list of results for display of autocomplete.
-//            var results = [];
-//            for (var i = 0; i < numRows; i++) {
-//                console.log(response.getDataTable().getValue(i, 0));
-//                results.push(response.getDataTable().getValue(i, 0));
-//            }
+            //            var results = [];
+            //            for (var i = 0; i < numRows; i++) {
+            //                console.log(response.getDataTable().getValue(i, 0));
+            //                results.push(response.getDataTable().getValue(i, 0));
+            //            }
         });
         //
         //        // Use the results to create the autocomplete options.
