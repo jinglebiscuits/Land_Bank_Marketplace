b913be5cc2a3c40aa661f0dc50d366e04687f6ad
jamall harris <jharri34@uab.edu>
2015-03-03 02:28:15 -0600

    cleaning out files

--- app/scripts/nav.js
+++ app/scripts/nav.js
@@ -1,16 +1,3 @@
-//$('#tableview').click(function () {
-//
-//    $("#table").toggle();
-//});
-//
-//$("#menu,.ul>li").click(function () {
-//    console.log("hey you click something");
-//    $('.active').removeClass('active');
-//    console.log(this);
-//    $(this).addClass('active');
-//});
-//var selector = ;
-
 $('.nav li').on('click', function(){
     $('.nav li').removeClass('active');
     $(this).addClass('active');
